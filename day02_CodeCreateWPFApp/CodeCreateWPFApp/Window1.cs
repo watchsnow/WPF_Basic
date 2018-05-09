129133f71eaa3c7bd3affb14e4e289dc64dda67d
wenht817 <wenht817@gmail.com>
2018-05-09 18:31:24 +0800

    CodeCreateWPFApp

---
+++ day02_CodeCreateWPFApp/CodeCreateWPFApp/Window1.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace CodeCreateWPFApp
+{
+    class Window1:Window
+    {
+        private Button btn1;
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        public void InitializeComponent()
+        {
+            this.Width = 285;
+            this.Height = 250;
+            this.Left = this.Top = 100;
+            this.Title = "Code-Only Window";
+            DockPanel pa = new DockPanel();
+            btn1 = new Button();
+            btn1.Content = "Please click me";
+            btn1.Margin = new Thickness(30);
+            btn1.Click += btn1_clieck;
+            IAddChild con = pa;
+            con.AddChild(btn1);
+            con = this;
+            con.AddChild(pa);
+        }
+        private void btn1_clieck(object sender, RoutedEventArgs enent)
+        {
+            btn1.Content = "thank you";
+        }
+    }
+}
---
+++ day02_CodeCreateWPFApp/CodeCreateWPFApp/program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CodeCreateWPFApp
+{
+    class program :Application
+    {
+        [STAThread()] // 单线程
+        static void Main()
+        {
+            program app = new program();
+            app.MainWindow = new Window1();
+            app.MainWindow.ShowDialog();
+        }
+    }
+}
